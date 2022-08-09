e9371e7a45e28042faf114f2cc3e8d241baed1b0
juan daniel suarez amado <62775732+jdvpl@users.noreply.github.com>
2022-08-09 00:02:40 -0500

    routing configuration

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-function App() {
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/home";
+
+const App = () => {
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline text-center mt-9 flex justify-center h-96 cursor-pointer text-yellow-400">
-        Hello world!
-      </h1>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
   );
-}
+};
 
 export default App;
