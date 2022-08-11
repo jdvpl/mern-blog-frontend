2dee3104a16d7d0a071ee0329640e146eaba4702
juan daniel suarez amado <62775732+jdvpl@users.noreply.github.com>
2022-08-10 23:02:39 -0500

    headless UI, admin public private navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
 import { Routes, Route } from "react-router-dom";
+import Header from "./components/navbar/Header";
 import Home from "./pages/home";
 import Login from "./pages/login/Login";
 import Register from "./pages/register";
+import { BrowserRouter as Router } from "react-router-dom";
 
 const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/login" element={<Login />} />
-    </Routes>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
   );
 };
 
---
+++ src/components/navbar/Header.jsx
@@ -0,0 +1,13 @@
+import AdminNavbar from "./Admin/AdminNavbar";
+import PrivateNavbar from "./private/PrivateNavbar";
+import PublicNavbar from "./public/PublicNavbar";
+
+const Header = () => {
+  return (
+    <>
+      <PublicNavbar />
+    </>
+  );
+};
+
+export default Header;
