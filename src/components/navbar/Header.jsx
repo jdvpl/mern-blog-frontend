7e67c402ee8be53603a59a343cf979573b6c7430
juan daniel suarez amado <62775732+jdvpl@users.noreply.github.com>
2022-08-10 23:10:27 -0500

    dinamic user

--- src/components/navbar/Header.jsx
+++ src/components/navbar/Header.jsx
@@ -1,11 +1,20 @@
 import AdminNavbar from "./Admin/AdminNavbar";
 import PrivateNavbar from "./private/PrivateNavbar";
 import PublicNavbar from "./public/PublicNavbar";
-
+import { useSelector } from "react-redux";
 const Header = () => {
+  const state = useSelector((state) => state?.users);
+  const { userAuth } = state;
+  const isAdmin = userAuth?.isAdmin;
   return (
     <>
-      <PublicNavbar />
+      {!userAuth ? (
+        <PublicNavbar />
+      ) : userAuth ? (
+        <PrivateNavbar />
+      ) : (
+        isAdmin && <AdminNavbar />
+      )}
     </>
   );
 };
